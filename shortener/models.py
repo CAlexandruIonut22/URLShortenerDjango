d3839fc507f2cb077edb7ce74d95de29f0beadc9
costa <costa@DESKTOP-BKU93S9>
2023-06-25 22:31:37 +0300

    Modified models

--- shortener/models.py
+++ shortener/models.py
@@ -2,13 +2,27 @@ from django.db import models
 from .utils import make_shortcode, create_shortcode
 
 
+class KirrURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(KirrURLManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+    def refresh_shortcodes(self):
+        qs = KirrURL.objects.all(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            new_codes += 1
+        return new_codes
 class KirrURL(models.Model):
     url = models.CharField(max_length=220)
     shortcode = models.CharField(max_length=8, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     test = models.CharField(max_length=3)
-
+    active = models.BooleanField(default=True)
+    objects = KirrURLManager()
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == "":
             self.shortcode = create_shortcode(self)
