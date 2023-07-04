b00554946c232f81aba868bdcdbdc176f5906f34
costa <costa@DESKTOP-BKU93S9>
2023-07-05 01:41:17 +0300

    Shortcodes and urls

--- kirr/urls.py
+++ kirr/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import re_path
 from shortener.views import kirr_redirect_view, KirrCBView
+from shortener import views
 
 urlpatterns = [
     re_path(r'^new-admin/', admin.site.urls),
-    re_path(r'^view-1/$', kirr_redirect_view),
-    re_path(r'^view-2/$', KirrCBView.as_view),
+    re_path(r'^a/(?P<shortcode>[\w-]+)/$', kirr_redirect_view),
+    re_path(r'^b/(?P<shortcode>[\w-]+)/$', KirrCBView.as_view()),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -3,10 +3,13 @@ from django.views import View
 
 
 # Create your views here.
-def kirr_redirect_view(request, *args, **kwargs):  # function based view FBV
-    return HttpResponse("hello")
+def kirr_redirect_view(request, shortcode=None, *args, **kwargs):  # function based view FBV
+    print(shortcode)
+    return HttpResponse(f"hello {shortcode}".format(shortcode=shortcode))
 
 
 class KirrCBView(View):  # class based view CBV
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("hello again")
+    def get(self, request, shortcode=None, *args, **kwargs):
+        print(shortcode)
+
+        return HttpResponse(f"hello again {shortcode}".format(shortcode=shortcode))
